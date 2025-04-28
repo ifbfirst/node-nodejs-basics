7670bbde5f092137f9637fab579302c5cb697c33
Kate Mashko <kat_zb@mail.ru>
2025-04-28 21:47:02 +0300

    fix: fix creating function

--- src/fs/create.js
+++ src/fs/create.js
@@ -5,18 +5,22 @@ import { fileURLToPath } from 'url';
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const create = async () => {
-    const filePath = path.join(__dirname, 'files', 'fresh.txt');
+    const dirPath = path.join(__dirname, 'files'); 
+    const filePath = path.join(dirPath, 'fresh.txt');
 
     try {
-        await fs.access(filePath).then(() => {
+        await fs.mkdir(dirPath, { recursive: true });
+        try {
+            await fs.stat(filePath);
             throw new Error('FS operation failed');
-        }).catch(() => {});
+        } catch (error) {
+            if (error.code !== 'ENOENT') throw error; 
+        }
 
-        await fs.mkdir(path.dirname(filePath), { recursive: true });
+
         await fs.writeFile(filePath, 'I am fresh and young');
     } catch (error) {
         console.error(error.message);
     }
 };
-
 create();
